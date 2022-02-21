70bde3e1580133e4323931b3d44c298e3ddafeee
Jacob Schwartz <jacob@jaschwartz.net>
2022-02-21 10:00:20 -0500

    Vectorize charging cost
    Co-authored-by: Qingyu Xu <xuqingyu0610@gmail.com>

--- src/write_outputs/write_outputs.jl
+++ src/write_outputs/write_outputs.jl
@@ -138,7 +138,7 @@ function write_outputs(EP::Model, path::AbstractString, setup::Dict, inputs::Dic
 	if has_duals(EP) == 1
 		dfPrice = write_price(path, inputs, setup, EP)
 		dfEnergyRevenue = write_energy_revenue(path, inputs, setup, EP, dfPower, dfPrice, dfCharge)
-		dfChargingcost = write_charging_cost(path, inputs, dfCharge, dfPrice, dfPower, setup)
+		dfChargingcost = write_charging_cost(path, inputs, setup, EP)
 		dfSubRevenue, dfRegSubRevenue = write_subsidy_revenue(path, inputs, setup, EP)
 	end
 
