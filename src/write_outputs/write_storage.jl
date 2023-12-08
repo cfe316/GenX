dd31dbd46d4ee2441a77693b228c7cab5da510f6
Jacob Schwartz <jacob@jaschwartz.net>
2023-12-07 21:29:43 -0500

    Contextualize energy_scale in write_storage

--- src/write_outputs/write_storage.jl
+++ src/write_outputs/write_storage.jl
@@ -12,26 +12,26 @@ function write_storage(path::AbstractString, inputs::Dict,setup::Dict, EP::Model
 	FLEX = inputs["FLEX"]
 	VRE_STOR = inputs["VRE_STOR"]
 	VS_STOR = !isempty(VRE_STOR) ? inputs["VS_STOR"] : []
+
+    energy_scale = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
 	
 	# Storage level (state of charge) of each resource in each time step
 	dfStorage = DataFrame(Resource = inputs["RESOURCES"], Zone = dfGen[!,:Zone])
 	storagevcapvalue = zeros(G,T)
 
-	if !isempty(inputs["STOR_ALL"])
+	if !isempty(STOR_ALL)
 	    storagevcapvalue[STOR_ALL, :] = value.(EP[:vS][STOR_ALL, :])
 	end
-	if !isempty(inputs["HYDRO_RES"])
+	if !isempty(HYDRO_RES)
 	    storagevcapvalue[HYDRO_RES, :] = value.(EP[:vS_HYDRO][HYDRO_RES, :])
 	end
-	if !isempty(inputs["FLEX"])
+	if !isempty(FLEX)
 	    storagevcapvalue[FLEX, :] = value.(EP[:vS_FLEX][FLEX, :])
 	end
 	if !isempty(VS_STOR)
 	    storagevcapvalue[VS_STOR, :] = value.(EP[:vS_VRE_STOR][VS_STOR, :])
 	end
-	if setup["ParameterScale"] == 1
-	    storagevcapvalue *= ModelScalingFactor
-	end
+    storagevcapvalue *= energy_scale
 
 	dfStorage = hcat(dfStorage, DataFrame(storagevcapvalue, :auto))
 	auxNew_Names=[Symbol("Resource");Symbol("Zone");[Symbol("t$t") for t in 1:T]]
