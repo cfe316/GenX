c9f4cb29317346c28477e2fa354ba5443a911888
Jacob Schwartz <jacob@jaschwartz.net>
2023-12-07 21:10:42 -0500

    Slightly more idiomatic loading

--- src/load_inputs/load_maximum_capacity_requirement.jl
+++ src/load_inputs/load_maximum_capacity_requirement.jl
@@ -7,12 +7,11 @@ function load_maximum_capacity_requirement!(path::AbstractString, inputs::Dict, 
     filename = "Maximum_capacity_requirement.csv"
     df = load_dataframe(joinpath(path, filename))
     inputs["NumberOfMaxCapReqs"] = nrow(df)
-    inputs["MaxCapReq"] = df[!, :Max_MW]
 
     scale_factor = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
 
-    inputs["MaxCapReq"] /= scale_factor
-    if "PriceCap" in names(df)
+    inputs["MaxCapReq"] = df[!, :Max_MW] / scale_factor
+    if hasproperty(df, :PriceCap)
         inputs["MaxCapPriceCap"] = df[!, :PriceCap] / scale_factor
     end
     println(filename * " Successfully Read!")
--- src/load_inputs/load_minimum_capacity_requirement.jl
+++ src/load_inputs/load_minimum_capacity_requirement.jl
@@ -6,17 +6,13 @@ Read input parameters related to mimimum capacity requirement constraints (e.g. 
 function load_minimum_capacity_requirement!(path::AbstractString, inputs::Dict, setup::Dict)
     filename = "Minimum_capacity_requirement.csv"
     df = load_dataframe(joinpath(path, filename))
-    NumberOfMinCapReqs = length(df[!,:MinCapReqConstraint])
-    inputs["NumberOfMinCapReqs"] = NumberOfMinCapReqs
-    inputs["MinCapReq"] = df[!,:Min_MW]
-    if setup["ParameterScale"] == 1
-        inputs["MinCapReq"] /= ModelScalingFactor # Convert to GW
-    end
-    if "PriceCap" in names(df)
-        inputs["MinCapPriceCap"] = df[!,:PriceCap]
-        if setup["ParameterScale"] == 1
-            inputs["MinCapPriceCap"] /= ModelScalingFactor # Convert to million $/GW
-        end
+    inputs["NumberOfMinCapReqs"] = nrow(df)
+
+    scale_factor = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
+
+    inputs["MinCapReq"] = df[!, :Min_MW] / scale_factor # Convert to GW
+    if hasproperty(df, :PriceCap)
+        inputs["MinCapPriceCap"] = df[!, :PriceCap] / scale_factor
     end
     println(filename * " Successfully Read!")
 end
